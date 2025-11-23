2dcd76bf7b95d4e2b7bb01e7eb023dd54686d7f2
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2025-11-23 21:14:42 +0100

    Update repasoinicial1.py

--- repasoinicial1.py
+++ repasoinicial1.py
@@ -1,4 +1,4 @@
-# Variables
+#Variables
 
 a = 10
 b = 10.5
@@ -10,9 +10,25 @@ print(b,type(b))
 print(c,type(c))
 print(d,type(d))
 
+#Control de acceso
+
 edad = int(input("Tu edad: "))
 
 if edad >= 18:
     print("Puedes entrar")
 else:
     print("No puedes entrar")
+
+#Suma de 5 numeros
+
+def es_par(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+n = int(input("Dime un numero:"))
+if es_par(n):
+    print("Es par")
+else:
+    print("Es impar")
