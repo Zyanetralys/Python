b319eb967c27b0821a61441b05a22e16b0c6c1e5
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2025-11-23 20:53:32 +0100

    Update and rename variables1.py to repasoinicial1.py

--- repasoinicial1.py
+++ repasoinicial1.py
@@ -9,3 +9,10 @@ print(a,type(a))
 print(b,type(b))
 print(c,type(c))
 print(d,type(d))
+
+edad = int(input("Tu edad: "))
+
+if edad >= 18:
+    print("Puedes entrar")
+else:
+    print("No puedes entrar")
