a87d727e5a0dfd420b9456fd4d54bd3e79ef0870
Zyanetralys <105277293+Zyanetralys@users.noreply.github.com>
2026-04-09 19:24:38 +0200

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,27 @@
+a = "Hola"
+print(a[2])
+
+for i in "Banana":
+    print(i)
+
+saludo = "Saludo"
+print(len(saludo))
+
+saludo2 = "Saludo a ti"
+print(len(saludo2))
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+
+txt = "The best things in life are !"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+else:
+  print("No, 'free' is NOT present.")
+
+
+txt = "The best things in life are free!"
+if "free" not in txt:
+  print("No, 'free' is not present.")
+
---
+++ ejercicio4strings.py
@@ -0,0 +1,12 @@
+name = "Maria Jesus"
+age = "34"
+
+print("Hola, soy {name} y tengo {age} años")
+
+print(f"Hola, soy {name} y tengo {age} años")
+
+operacion1=7/2
+print(f"dividiendo con / el resultado es:{operacion1}") 
+
+operacion2=7//2
+print(f"dividiendo con // el resultado es:{operacion2}")
